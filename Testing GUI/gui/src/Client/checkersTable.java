59058fddab07c39e675b42aaef5a9e77ab11fb59
jeremy00 <jwmayunoh@gmail.com>
2014-11-17 17:20:26 -0500

    game table working on
    Coloring the game table

--- Testing GUI/gui/src/Client/checkersTable.java
+++ Testing GUI/gui/src/Client/checkersTable.java
@@ -1,21 +1,32 @@
 package Client;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
+
 import java.awt.Color;
+
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JList;
+
 import java.awt.GridLayout;
+
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
+//import javax.swing.text.html.AccessibleHTML.TableElementInfo.TableAccessibleContext;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.JLabel;
 import javax.swing.border.LineBorder;
 import javax.swing.ListSelectionModel;
@@ -62,18 +73,38 @@ public class checkersTable extends JFrame {
 		btnNewButton.setBounds(508, 353, 89, 23);
 		contentPane.add(btnNewButton);
 		
+		
+		
 		JTextArea textArea = new JTextArea();
 		textArea.setBounds(10, 266, 587, 78);
 		contentPane.add(textArea);
 		
+		
+		
+		
 		JList list = new JList();
-		list.setBounds(488, 35, 126, 147);
+		list.setBounds(512, 65, 126, 147);
 		contentPane.add(list);
 		
-		table = new JTable();
+		table = new JTable(){
+			public Component prepareRenderer(TableCellRenderer r, int rw, int col)
+			{
+				Component c = super.prepareRenderer(r, rw,  col);
+				c.setBackground(Color.WHITE);
+				if (col==0){
+					c.setBackground(Color.GREEN);
+				}
+				return c;
+			}
+		};
+		
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setCellSelectionEnabled(true);
 		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		
+		
+		
+		
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 				{null, null, null, null, null, null, null, null},
@@ -89,7 +120,10 @@ public class checkersTable extends JFrame {
 				"New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column"
 			}
 		));
-		table.setBounds(113, 84, 374, 147);
+		
+		
+		
+		table.setBounds(107, 84, 388, 128);
 		contentPane.add(table);
 		
 		JButton btnLeave = new JButton("Leave");
