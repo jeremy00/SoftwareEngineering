a735d7b48f1a3da01a8fd6dedd6ef4660305360a
jeremy00 <jwmayunoh@gmail.com>
2014-11-17 08:31:18 -0500

    Rough Sketch of table GUI

--- Testing GUI/gui/src/Client/checkersTable.java
+++ Testing GUI/gui/src/Client/checkersTable.java
@@ -6,10 +6,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
+import java.awt.GridLayout;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
+import javax.swing.ListSelectionModel;
 
 public class checkersTable extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +47,65 @@ public class checkersTable extends JFrame {
 	 */
 	public checkersTable() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 640, 439);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 354, 485, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(508, 353, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 266, 587, 78);
+		contentPane.add(textArea);
+		
+		JList list = new JList();
+		list.setBounds(488, 35, 126, 147);
+		contentPane.add(list);
+		
+		table = new JTable();
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setCellSelectionEnabled(true);
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		table.setBounds(113, 84, 374, 147);
+		contentPane.add(table);
+		
+		JButton btnLeave = new JButton("Leave");
+		btnLeave.setBounds(0, 159, 89, 23);
+		contentPane.add(btnLeave);
+		
+		JButton button = new JButton("Surrender");		
+		button.setBounds(0, 125, 89, 23);
+		contentPane.add(button);
+		
+		JLabel lblTable = new JLabel("table #");
+		lblTable.setBounds(113, 11, 46, 14);
+		contentPane.add(lblTable);
+		
+		JLabel lblRedsTurn = new JLabel("Red's Turn");
+		lblRedsTurn.setBounds(113, 46, 46, 14);
+		contentPane.add(lblRedsTurn);
 	}
-
 }
