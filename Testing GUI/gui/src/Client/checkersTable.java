72def819ad72ab971b2cc7433f40421c13b84c15
jeremy00 <jwmayunoh@gmail.com>
2014-11-28 12:07:31 -0500

    set the first piece on table

--- Testing GUI/gui/src/Client/checkersTable.java
+++ Testing GUI/gui/src/Client/checkersTable.java
@@ -32,12 +32,12 @@ import javax.swing.border.LineBorder;
 import javax.swing.ListSelectionModel;
 import java.awt.FlowLayout;
 import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class checkersTable extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTable table;
+	private JLabel table;
 
 	/**
 	 * Launch the application.
@@ -65,63 +65,28 @@ public class checkersTable extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(14, 75, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(105, 73, 89, 23);
-		contentPane.add(btnNewButton);
-		
-		
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(199, 74, 4, 22);
-		contentPane.add(textArea);
-
-	    JList list = new JList();
-	    list.setBounds(208, 85, 0, 0);
-		contentPane.add(list);
-		
-		     JLabel    red = new javax.swing.JLabel();
-			red.setBounds(249, 130, 42, 38);
-			 
-			 
-			     red.setIcon(new ImageIcon("C:\\Users\\Meri\\Desktop\\red.png"));
-			     contentPane.add(red);
-			
-		JButton btnLeave = new JButton("Leave");
-		btnLeave.setBounds(366, 73, 61, 23);
-		contentPane.add(btnLeave);
-		
-		JButton button = new JButton("Surrender");
-		button.setBounds(432, 73, 81, 23);
-		contentPane.add(button);
-		
-		JLabel lblTable = new JLabel("table #");
-		lblTable.setBounds(518, 78, 35, 14);
-		contentPane.add(lblTable);
-		
-		JLabel lblRedsTurn = new JLabel("Red's Turn");
-		lblRedsTurn.setBounds(558, 78, 51, 14);
-		contentPane.add(lblRedsTurn);
-		
-		
-		
-		 JLabel    jLabel1 = new javax.swing.JLabel();
-		 jLabel1.setBounds(249, 130, 408, 404);
-		 
-		 
-		     jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Meri\\Desktop\\ds32352.gif"));
-		     contentPane.add(jLabel1);
+		          
+		          
+		          
+		           //JLabel    table = new javax.swing.JLabel();
+		           
+		           table.setHorizontalAlignment(SwingConstants.TRAILING);
+		           table.setBounds(20, 11, 408, 404);
+		           
+		           
+		               table.setIcon(new javax.swing.ImageIcon("C:\\Users\\Meri\\Desktop\\ds32352.gif"));
+		               contentPane.add(table);
+		             
+		            
+		             
+		     System.out.println("add piece");
 		     
-		     JLabel label = new JLabel();
-		     label.setBounds(310, 493, 42, 38);
-		     contentPane.add(label);
 	}
-//private JLabel returnRed(){
-//	
-//}
+	
+	public void setTable(){
+		  piece p = new piece(true, table);
+          
+          table.add(p);
+	}
+	
 }
--- Testing GUI/gui/src/Client/piece.java
+++ Testing GUI/gui/src/Client/piece.java
@@ -1,6 +1,9 @@
 package Client;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 //set 
 public class piece extends JLabel{
@@ -10,12 +13,21 @@ public class piece extends JLabel{
 	int yDiff =53;//difference in spots on the board
 	int xDiff =50;//difference in spots on the board
 	int tableX, tableY;
+	JLabel table;
 	
 	String pathIcon;
-	piece(boolean color, JLabel table){
-		tableX = table.getX();
+	piece(boolean red, JLabel table){
+		this.table = table;
+		tableX = table.getX();//retrieve the table coords
 		tableY = table.getX();
-			}
+		x = 0;
+		y = 0;
+		//set the image icon of the JLabel
+		if (red)this.setIcon(new ImageIcon("C:\\Users\\Meri\\Desktop\\red.png"));
+		System.out.println("piece x: " + x  +" piece y: " + y)	;
+		this.setBounds(x, y, 50, 50);//set the position 
+//		  table.setHorizontalAlignment(SwingConstants.);
+	}
 	
 	//set the position of the checkers piece using 
 	//@param boardX and boardY are the coordinates on the board
@@ -24,11 +36,8 @@ public void setPiece(int boardX, int boardY){
 								//add the diff(between spots) * that spot on the board (0-8)
 	this.x = tableY + (yDiff*y);
 	
-	this.setLocation(x, y);
+	this.setLocation(x, y);//set the location of the JLabel on the table
+	System.out.format("x: %d y: %d", x,y);
 }
-	public void setX(int x, int y){
-		
-	}
-	
 	
 }
