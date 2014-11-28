f9883228c0b80b6f7847f78e4dcf49aa8768d306
jeremy00 <jwmayunoh@gmail.com>
2014-11-28 13:36:14 -0500

    Added the GUI back, commented stuff, started move pieces

--- Testing GUI/gui/src/Client/checkersTable.java
+++ Testing GUI/gui/src/Client/checkersTable.java
@@ -7,11 +7,23 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
+import java.awt.Font;
 
+
+//TOP LEFT IS 0,0
 public class checkersTable extends JFrame {
 
 	private JPanel contentPane;
-	private JLabel table;
+	private JTextField ChatInputField;
+	private JList UserList;
+	private JButton btnStart,btnLeave;
+	private JLabel lblUserList, lblTableNum,lblConsole, table;
+	private piece[][] piecesArray;
 
 	/**
 	 * Launch the application.
@@ -33,27 +45,87 @@ public class checkersTable extends JFrame {
 	 * Create the frame.
 	 */
 	public checkersTable() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
-		table = new javax.swing.JLabel();
-
-		table.setHorizontalAlignment(SwingConstants.TRAILING);
-		table.setBounds(20, 11, 408, 404);
-
-		table.setIcon(new ImageIcon(checkersTable.class
-				.getResource("/Client/table.gif")));
-		contentPane.add(table);
+		piecesArray = new piece[7][7];
+		
+		setGUI();
 		setTable();
 
-		System.out.println("add piece");
-
+		
 	}
+public void setGUI(){
+	//Setup the JPANEL
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setBounds(100, 100, 800, 730);
+	contentPane = new JPanel();
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+	contentPane.setLayout(null);
 
+	//Set all of the options, then add it to the panel
+	//Table
+	table = new javax.swing.JLabel();
+	table.setHorizontalAlignment(SwingConstants.TRAILING);
+	table.setBounds(187, 83, 408, 404);
+	table.setIcon(new ImageIcon(checkersTable.class
+			.getResource("/Client/table.gif")));
+	
+	//Chat Input
+	ChatInputField = new JTextField("");
+	ChatInputField.setColumns(10);
+	ChatInputField.setBounds(10, 652, 585, 20);
+	
+	//Submit Button
+	JButton btnSubmit = new JButton("Submit");
+	btnSubmit.setFont(new Font("Times New Roman", Font.BOLD, 15));
+	btnSubmit.setBounds(619, 651, 155, 35);
+	
+	//Chat Area
+	JTextArea ChatArea = new JTextArea("");
+	ChatArea.setForeground(Color.BLACK);
+	ChatArea.setBounds(10, 498, 764, 143);
+	
+	
+	//User List
+	UserList = new JList();
+	UserList.setBounds(605, 122, 169, 319);
+	
+	//Start Button
+	btnStart = new JButton("Start");
+	btnStart.setFont(new Font("Times New Roman", Font.BOLD, 20));
+	btnStart.setBounds(10, 142, 124, 68);
+	//Leave Button
+	btnLeave = new JButton("Leave");
+	btnLeave.setFont(new Font("Times New Roman", Font.BOLD, 20));
+	btnLeave.setBounds(10, 232, 124, 68);
+	//User List
+	lblUserList = new JLabel("User List");
+	lblUserList.setFont(new Font("Times New Roman", Font.BOLD, 19));
+	lblUserList.setBounds(652, 70, 150, 53);
+	//Table Number
+	lblTableNum = new JLabel("Table #:");
+	lblTableNum.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+	lblTableNum.setBounds(230, 11, 322, 14);
+	//Console to let user know who's turn and what the status of the game is.
+	lblConsole = new JLabel("Start Game");
+	lblConsole.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+	lblConsole.setBounds(230, 45, 322, 14);
+    
+	
+	//Now add everything to the table
+	contentPane.add(table);		
+	contentPane.add(ChatInputField);
+	contentPane.add(btnSubmit);
+	contentPane.add(ChatArea);
+	contentPane.add(UserList);
+	contentPane.add(btnStart);
+	contentPane.add(btnLeave);
+	contentPane.add(lblUserList);
+	contentPane.add(lblTableNum);
+	contentPane.add(lblConsole);
+	
+
+	
+}
 	// Set the table up for the first run.
 	//Runs from the top left to the bottom right.
 	public void setTable() {
@@ -61,7 +133,7 @@ public class checkersTable extends JFrame {
 
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
-
+				
 				//Set first three rows, stop, then set last three rows
 				if (i < 3)
 					red = true;
@@ -71,11 +143,14 @@ public class checkersTable extends JFrame {
 					break;
 
 				//If it's a checkers position that needs a piece.
-				if (i % 2 == 0 && j % 2 == 0)
+				if (i % 2 == 0 && j % 2 == 0){
+					
 					setTablePiece(i, j, red);
-
-				else if (i % 2 == 1 && j % 2 == 1)
+				}
+				else if (i % 2 == 1 && j % 2 == 1){
+					
 					setTablePiece(i, j, red);
+			}
 			}// end row
 		}
 	}
@@ -86,6 +161,13 @@ public class checkersTable extends JFrame {
 		piece p = new piece(red, table);
 		p.setPiece(i, j);
 		table.add(p);//add the the GUI
+		piecesArray[i][j] = p;
 	}
-
+	
+	//Called when moving from spot to spot. FROM TOP LEFT is 0,0
+	public void movePiece(int fromRow, int fromCol, int toRow, int toCol){
+		
+		//set the piece when it's been moved..
+		piecesArray[toRow-1][toCol-1].setPiece(toRow, toCol);
+	}
 }
